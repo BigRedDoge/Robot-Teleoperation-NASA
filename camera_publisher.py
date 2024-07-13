02d11921072a6ee1d23b58a3b11b40753b2b1c57
Sean Clifford <seancliff01@gmail.com>
2024-07-13 15:29:27 -0400

    converted from ros 1 to ros 2

--- camera_publisher.py
+++ camera_publisher.py
@@ -1,48 +1,41 @@
-import rospy
+from rclpy.node import Node
 from camera.msg import Image
 import cv2
+import rclpy
 
+class CameraPublisher(Node):
+    """
+    Publishes the camera feed to the 'camera_feed' topic
+    """
+    def __init__(self):
+        super().__init__('camera_publisher')
+        self.publisher_ = self.create_publisher(Image, 'camera_feed', 10)
+        self.timer = self.create_timer(0.1, self.publish_camera_feed)  # 10 Hz
+        self.cap = cv2.VideoCapture(0)
 
-"""
-Publishes the camera feed to the 'camera_feed' topic
-"""
-def publish_camera_feed():
-    # Initialize the publisher
-    pub = rospy.Publisher('camera_feed', Image, queue_size=10)
-
-    # Initialize the node
-    rospy.init_node('camera_publisher', anonymous=True)
-
-    # Open the camera
-    cap = cv2.VideoCapture(0)
-
-    # Set the rate
-    rate = rospy.Rate(10)  # 10 Hz
-
-    while not rospy.is_shutdown():
-        # Capture a frame
-        ret, frame = cap.read()
-
+    def publish_camera_feed(self):
+        ret, frame = self.cap.read()
         if ret:
-            # Convert the frame to a ROS Image message
-            image = cv2.imencode('.jpg', frame)[1]
-            image_bytes = image.tobytes()
-
+            frame_bytes = cv2.imencode('.jpg', frame)[1].tobytes()
+            # Put the frame in a Image message
             msg = Image()
-            msg.data = image_bytes
-
+            msg.data = frame_bytes
             # Publish the message
-            pub.publish(msg)
+            self.publisher_.publish(msg)
 
-        # Sleep for the remaining time
-        rate.sleep()
 
-    # Release the camera
-    cap.release()
+def main(args=None):
+    rclpy.init(args=args)
+    camera_publisher = CameraPublisher()
+    try:
+        rclpy.spin(camera_publisher)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        # Destroy the node explicitly
+        camera_publisher.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    try:
-        publish_camera_feed()
-    except rospy.ROSInterruptException:
-        pass
+    main()
